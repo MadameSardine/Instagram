47b723a98ebdb36968945a0322767ad70a25104e
Sandrine <sandrine.zhangferron@gmail.com>
2014-11-18 18:56:53 +0000

    Added delete button for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,13 @@ class PostsController < ApplicationController
     redirect_to '/posts'
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "Post deleted successfully"
+    redirect_to '/posts'
+  end
+
   def post_params
     params.require(:post).permit(:image)
   end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -63,3 +63,44 @@ describe 'Posting new photos' do
     end
   end
 end
+
+describe 'Deleting posts' do
+
+  before do
+    @user = User.create(email: 'test@test.com', password: 'password', password_confirmation: 'password')
+    @user2 = User.create(email: 'test2@test.com', password: 'password', password_confirmation: 'password')
+  end
+
+  context 'User not logged in' do
+
+    it "should not be able to delete a post" do
+      visit '/posts'
+      expect(page).not_to have_link 'Delete'
+    end
+  end
+
+  context 'User logged in' do
+
+    it "should not be able to delete someone else's post" do
+      login_as @user
+      visit '/posts'
+      click_link 'Post'
+      attach_file 'post_image', 'spec/helpers/images/baby-duck.jpg'
+      click_button 'Upload image'
+      login_as @user2
+      visit '/posts'
+      expect(page).not_to have_content 'Delete'
+    end
+
+    it "should be able to delete his own post" do
+      login_as @user
+      visit '/posts'
+      click_link 'Post'
+      attach_file 'post_image', 'spec/helpers/images/baby-duck.jpg'
+      click_button 'Upload image'
+      expect(page).to have_content 'Delete'
+      click_link 'Delete'
+      expect(page).to have_content 'Post deleted successfully'
+    end
+  end
+end
